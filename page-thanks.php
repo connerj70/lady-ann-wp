1a04146868f764dcdecfe27e4e3750fa3c87a167
Conner Jensen <connerj70@gmail.com>
2018-08-13 08:32:20 -0600

    w

---
+++ page-thanks.php
@@ -0,0 +1,27 @@
+<?php
+/**
+Template Name: Thanks Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package lady-ann
+ */
+
+?>
+
+	<div style="display: flex; align-items: center; justify-content: center; width: 100%; padding-top: 80px; flex-direction: column;">
+		<img style="width: 350px;" src="<?php bloginfo('template_url'); ?>/assets/faded-logo.svg" />
+		<h1 style="margin-bottom: 10px; margin-top: 80px; font-size: 50px; font-family: sans-serif; font-weight: 100;">Thanks For Sharing!</h1>
+		<h3 style="text-align: center; font-size: 25px; font-family: sans-serif; font-weight: 100; opacity: 0.6;">We appreciate your sharing your story with us. <br /> Watch out for you letter to be posted under our <a style="color: #ea8b88; text-decoration: none;"href="lady-ann/letters">Letters section</a> </h3>
+		<h3 style="text-align: center; font-size: 25px; font-family: sans-serif; font-weight: 100; opacity: 0.6;">Like what we do? Click the share button to tell your friends about us.</h3>
+		<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+		    <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+		</div>
+
+		<h3 style="margin-bottom: 0; text-align: center; font-size: 25px; font-family: sans-serif; font-weight: 100; opacity: 0.6;"> #DearLadyAnn #WeLoveLetters</h3>
+		
+		<script async src="https://static.addtoany.com/menu/page.js"></script>
+
+	</div><!-- #primary -->
+
+<?php
